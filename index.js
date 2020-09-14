65f79089fc650680dab1d7c1cd7bc49ed7a059f9
Hanif adedotun <hanif.adedotun@gmail.com>
2020-09-14 12:01:09 +0100

    enter

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express');
+// const express = require('express');
 
+// const app = express();
+// app.use(express.static('static'));
+// app.listen(8080, function () {
+// console.log('App started on port 3000');
+// });
+const express = require('express');
 const app = express();
-app.use(express.static('static'));
-app.listen(8080, function () {
-console.log('App started on port 3000');
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, This is Hanif nodejs');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
