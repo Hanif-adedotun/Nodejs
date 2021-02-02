2387f74530db3987548c32201b1ffe730bbf6817
Hanif adedotun <hanif.adedotun@gmail.com>
2021-02-02 22:33:10 +0100

    Improved the footer

--- voltex_react-app/src/components/Footer/footer.js
+++ voltex_react-app/src/components/Footer/footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './footer.css';
-import image from '../images/voltex.png';
+import logo from '../images/voltex.png';
+
+//Import bootstrap 3
+import '../css/bootstrap.min.css';
 
 var year = new Date().getFullYear();
 class footer extends React.Component{
@@ -8,19 +11,28 @@ class footer extends React.Component{
         return(
             <div className='footer'>
                 <span>
-                    <img className='footimg' src={image} alt='Voltex logo'></img>   
+                    <img className='footimg' src={logo} alt='Voltex logo'></img>   
                 </span>
-                <div id='contactLinks'>
+                <div className='row'>
+                <div className='col-md-6 foot' id='contactLinks'>
                     <label>Contact US</label>
                     <ul>
-                        <li><a href="#">Github</a></li>
-                        <li><a href="#">twitter</a></li>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">E-mail</a></li>
+                        <li><a >Github</a></li>
+                        <li><a >twitter</a></li>
+                        <li><a >Instagram</a></li>
+                        <li><a >E-mail</a></li>
                     </ul>
                 </div>
+                <div className='col-md-6 foot' id='policies'>
+                <label>Policies</label>
+                    <ul>
+                        <li><a>Private policies</a></li>
+                        <li><a>Lincense</a></li>
+                    </ul>
+                </div>
+                </div>
                 <p className='footext'>&copy; Copyright Voltex Designs {year}</p>
-                
+            
             </div>
         )
     }
